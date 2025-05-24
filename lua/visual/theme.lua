8d3845e121321371c41c83c7ce327c2e79dc9ed3
nandesh-dev <nandesh.dev@gmail.com>
2025-05-24 15:25:24 +0000

    Switch to evergarden spring theme

--- lua/visual/theme.lua
+++ lua/visual/theme.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         require("evergarden").setup({
             theme = {
-                variant = "winter",
+                variant = "spring",
             },
         })
         vim.cmd("colorscheme evergarden")
