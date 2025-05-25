35bda25ddf14e8ff9ef595088cd5e144a3daf6a5
nandesh-dev <nandesh@gmail.com>
2025-05-25 09:03:13 +0000

    Add harpoon_files for lualine

--- lua/visual/statusline.lua
+++ lua/visual/statusline.lua
@@ -1,14 +1,31 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch" },
-            lualine_c = { "filename" },
-            lualine_x = { "filetype" },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        {
+            "kiennt63/harpoon-files.nvim",
+            dependencies = {
+                { "ThePrimeagen/harpoon" },
+            },
+            opts = {
+                show_icon = false,
+                separator_left = "",
+                separator_right = "",
+            },
         },
     },
+    config = function()
+        local harpoon_files = require("harpoon_files")
+
+        require("lualine").setup({
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = { { harpoon_files.lualine_component } },
+                lualine_x = { "filename" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        })
+    end,
 }
